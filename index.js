19538b369a01ff45b94697205223ee68eff447f3
zunairamarryum <zunairamarryum111@gmail.com>
2025-03-02 14:20:15 +0500

    refresh

--- index.js
+++ index.js
@@ -2,27 +2,25 @@ let slideBtnleft = document.getElementById("slide-btn-left");
 let slideBtnright = document.getElementById("slide-btn-right");
 let imgitem = document.querySelectorAll(".image-item");
 
-console.log(imgitem.length - 1);
+console.log(imgitem.length - 1)
 
-let startslider = 0;
+let startslider = 0
 let endslider = (imgitem.length - 1) * 100; // 700
 
-slideBtnleft.addEventListener("click", () => {
-    if (startslider < 0) {
-        startslider += 100;
+slideBtnleft.addEventListener("click",() =>{
+    if (startslider < 0){
+        startslider = startslider + 100;
+    }
+    imgitem.forEach(element =>{
+        element.style.transform = `translateX(${startslider}%)`;
+    })
+})
+slideBtnright.addEventListener("click",()=>{
+    if (startslider >= -endslider + 100){
+        startslider =startslider - 100;
     }
 
-    imgitem.forEach(element => {
+    imgitem.forEach(element =>{
         element.style.transform = `translateX(${startslider}%)`;
-    });
-});
-
-slideBtnright.addEventListener("click", () => {
-    if (startslider > -endslider + 100) {
-        startslider -= 100;
-    }
-
-    imgitem.forEach(element => {
-        element.style.transform = `translateX(${startslider}%)`;
-    });
-});
+    })
+})
